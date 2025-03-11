f5fab3c3d935107b937aba8c56f5f04185010700
yifeng <2108546503@qq.com>
2025-03-11 17:08:20 +0800

    feat: 自动化部署2.1

--- src/page/home/home.tsx
+++ src/page/home/home.tsx
@@ -1,3 +1,11 @@
+/*
+ * @Author: yifeng 2108546503@qq.com
+ * @Date: 2025-03-06 10:03:37
+ * @LastEditors: yifeng 2108546503@qq.com
+ * @LastEditTime: 2025-03-11 16:35:19
+ * @FilePath: \font\src\page\home\home.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import React, { useEffect } from "react";
 import request from "../../utils/axios/axios";
 import { wordFindList } from "../../server/word/word";
