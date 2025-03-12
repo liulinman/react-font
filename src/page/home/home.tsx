199f32640ac6ba7abb308a81d54875c246a7ffcb
yifeng <2108546503@qq.com>
2025-03-12 10:34:01 +0800

    feat: 自动化部署测试

--- src/page/home/home.tsx
+++ src/page/home/home.tsx
@@ -1,11 +1,3 @@
-/*
- * @Author: yifeng 2108546503@qq.com
- * @Date: 2025-03-06 10:03:37
- * @LastEditors: yifeng 2108546503@qq.com
- * @LastEditTime: 2025-03-11 16:35:19
- * @FilePath: \font\src\page\home\home.tsx
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 import React, { useEffect } from "react";
 import request from "../../utils/axios/axios";
 import { wordFindList } from "../../server/word/word";
@@ -20,7 +12,7 @@ const Home: React.FC = () => {
 
   return (
     <div>
-      首页<span>自动化部署1.1</span>
+      首页<span>自动化部署1.2</span>
     </div>
   );
 };
